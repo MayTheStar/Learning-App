bdddd89f66ce422bb2d0db597ba44a1193812e1f
MayTheStar <maibadr200@gmail.com>
2024-10-23 18:16:24 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by May Bader Alotaibi on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningTests/LearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningTests.swift
+//  LearningTests
+//
+//  Created by May Bader Alotaibi on 20/04/1446 AH.
+//
+
+import Testing
+@testable import Learning
+
+struct LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
